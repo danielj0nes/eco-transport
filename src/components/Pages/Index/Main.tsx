d807b59b39aceb9e3efa5e35abf9faef1e4a26c3
Nit <66027553+Nitlix@users.noreply.github.com>
2023-10-26 16:40:28 +0100

    [/] Final functionality updates

--- src/components/Pages/Index/Main.tsx
+++ src/components/Pages/Index/Main.tsx
@@ -5,10 +5,11 @@ import wrapperStyles from "./Wrapper.module.scss"
 import styles from "./Main.module.scss"
 
 
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from 'react';
 import { Location, Place, TravelType } from "@/var/types";
 import locationAutoComplete from "@/modules/locationAutoComplete";
 import calculateDistance from "@/modules/calculateDistance";
+import { ChartOptions } from "chart.js/auto";
 
 import dynamic from "next/dynamic";
 
@@ -17,7 +18,28 @@ const ReactOdometer = dynamic(
     { ssr: false }
 )
 
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    BarElement,
+    BarController,
+    Title
+);
+
 import "../../../lib/odometer.scss"
+import { Bar } from "react-chartjs-2";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    BarElement,
+    BarController,
+    Title
+} from 'chart.js';
 
 const selectors: { icon: string, name: TravelType, action: () => void }[] = [
     {
@@ -151,41 +173,189 @@ function Selector(
 }
 
 
+
+export const options: ChartOptions<"bar"> = {
+    indexAxis: 'y' as const,
+    maintainAspectRatio: false,
+    elements: {
+        
+        bar: {
+            
+        }
+    },
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'right' as const,
+        },
+            title: {
+            display: true,
+            
+            text: 'Emmissions produced in comparison to other travel types.',
+        },
+
+    },
+    font: {
+        size: 16,
+        family: "Montserrat",
+        weight: "bold"
+    },
+    //use 1s ease in out
+    transitions: {
+        x: {
+            animation: {
+                easing: 'easeInOutCubic',
+                duration: 2000
+            }
+        }
+    },
+
+    //label x and y axis
+    scales: {
+        y: {
+            title: {
+                display: true,
+                text: "Type of travel"
+            }
+        },
+        x: {
+            title: {
+                display: true,
+                text: "CO₂ Emissions (kg)"
+            }
+        }
+    }
+};
+
+  
+
 function Results(
     {
         distance, 
         footprint,
-        visible
+        visible,
+        travelType,
+        closeFunction
     }: 
     {
         distance: number,
         footprint: number,
-        visible: boolean
+        visible: boolean,
+        travelType: TravelType,
+        closeFunction: () => void
     }): JSX.Element 
+
+
+    
 {
-    return <div className={`flex v ${styles.results} ${visible ? "" : styles.hidden}`}>
-        <h2 style={{margin: "2rem 0"}}>
+    const animate = visible ? "aos-animate" : "";
+    const refs = [useRef<HTMLDivElement>(null), useRef<HTMLDivElement>(null), useRef<HTMLDivElement>(null), useRef<HTMLDivElement>(null), useRef<HTMLDivElement>(null)];
+    const resultsRef = useRef<HTMLDivElement>(null);
+
+    useEffect(()=>{
+        refs.forEach((ref, key)=>{
+            ref.current?.classList.remove("aos-animate");
+        })
+    }, [])
+
+    useEffect(()=>{
+
+        
+        
+        if (visible){
+            resultsRef.current?.classList.add(styles.resultsShow);
+            setTimeout(()=>{
+                resultsRef.current?.classList.add(styles.resultsVisible);
+            },50)
+
+            setTimeout(()=>{
+                refs.forEach((ref, key)=>{
+                    ref.current?.classList.add("aos-animate");
+                })
+            }, 500)  
+        }
+
+        else {
+            resultsRef.current?.classList.remove(styles.resultsVisible);
+            setTimeout(()=>{
+                resultsRef.current?.classList.remove(styles.resultsShow);
+            },500)
+        }
+        
+    }, [visible])
+    
+
+    const travelTypes = ["train", "bus", "car", "plane"];
+    const travelEmmisions = [0.041, 0.105, 0.192, 0.255]
+
+    const index = travelTypes.indexOf(travelType);
+
+    const data = {
+        labels: [],
+        datasets: [
+            {
+                label: "CO2 Emissions",
+                data: [],
+                backgroundColor: [
+                    
+                ],
+            },
+        ],
+    };
+
+
+    for (let i = 0; i < travelTypes.length; i++){
+        const display = travelTypes[i][0].toUpperCase() + travelTypes[i].slice(1);
+        data.labels.push(display as never);
+        data.datasets[0].data.push(travelEmmisions[i] * distance as never);
+        let color: string = "#E84A4A70";
+        if (index == i) color = "#E84A4A";
+        data.datasets[0].backgroundColor.push(color as never);
+    }
+
+
+
+    
+
+    return <div className={`flex v ${styles.results}`} ref={resultsRef}>
+        <h2 style={{margin: "2rem 0"}} className={`${animate}`} data-aos="fade-up" data-aos-delay="" ref={refs[0]} >
             Look what you did to the world!
         </h2>
         <div className={styles.resultsGrid}>
-            <div className={styles.resultsObject}>
+            <div className={styles.resultsObject} ref={refs[1]} data-aos="fade-up" data-aos-delay="100">
                 <img src="https://images.immediate.co.uk/production/volatile/sites/7/2018/02/Earth-from-space-1-64e9a7c.jpg?resize=768,574" />
                 <h3>
-                    You've travelled <span className="highlight"><ReactOdometer value={parseInt((distance).toFixed(0))} duration={1000} />km*</span>
+                    You've travelled <span className="highlight"><ReactOdometer value={parseInt((distance).toFixed(0))} duration={1000} />km.</span>
                 </h3>
             </div>
-            <div className={styles.resultsObject}>
+            <div className={styles.resultsObject} ref={refs[2]} data-aos="fade-up" data-aos-delay="200">
                 <img src="https://scx2.b-cdn.net/gfx/news/2023/carbon-emissions-cost.jpg" />
                 <h3>
-                    Your travels contributed to the release of <span className="highlight"><ReactOdometer value={parseFloat(footprint.toFixed(0))} duration={1000} />kg*</span> of CO2 into the atmosphere
+                    Your travels contributed to the release of <span className="highlight"><ReactOdometer value={parseFloat(footprint.toFixed(0))} duration={1000} />kg</span> of CO2 into the atmosphere
                 </h3>
             </div>
-            <div className={styles.resultsObject}>
+            <div className={styles.resultsObject} ref={refs[3]} data-aos="fade-up" data-aos-delay="300">
                 <img src="https://www.lse.ac.uk/granthaminstitute/wp-content/uploads/2018/01/CongoBrazza-credit-Bobulix-Flickr-e1676028067118.jpg" />
                 <h3 style={{display: "inline"}}>
                     Over <span className="highlight"><ReactOdometer value={parseInt((footprint/0.068).toFixed(0))} duration={1000} /> trees</span> would be needed to offset your carbon footprint.
                 </h3>
             </div>
+            <div className={`${styles.resultsObject} ${styles.resultsObjectChart}`} ref={refs[4]} data-aos="fade-up" data-aos-delay="400">
+                <Bar data={data} options={options} style={{width: "100%", height: "100%"}} />
+                <h3>
+                    {
+                        (index == 0) ? "You used the best travel method in terms of CO₂ emissions." :
+                        "You could've used a better travel method in terms of CO₂ emissions."
+                    }
+                </h3>
+                <button className={`flex center gap05`} onClick={closeFunction}>
+                    <i className='bx bxs-chevrons-right' />
+                    <h3>
+                        Try another trip?
+                    </h3>
+                </button>
+            </div>
+           
         </div>
 
     </div>
@@ -210,6 +380,8 @@ export default function (): JSX.Element {
 
 
     async function check(){
+        if (origin.id === "0" || destination.id === "0") return;
+
         const {
             distance,
             footprint
@@ -232,7 +404,7 @@ export default function (): JSX.Element {
                 Depending on how many kilometers you travel by car or by plane, we calculate the amount of CO2 emissions you produced and the consequences for our loved world.
             </p>
             <Selector originSetter={setOrigin} destinationSetter={setDestination} travelTypeSetter={setTravelType} travelType={travelType} checkFunction={check} data-aos="fade-up" data-aos-delay="200" />
-            <Results distance={distance} footprint={footprint} visible={resultsVisible} />
+            <Results distance={distance} footprint={footprint} visible={resultsVisible} travelType={travelType} closeFunction={()=>{setResultsVisible(false)}} />
         </div>
     </div>
 }
